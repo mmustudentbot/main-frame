6d14342b76a607cad0157c6b18326ba08ccadf6c
Umair Rehman <umairrehman66@gmail.com>
2023-03-23 14:52:28 +0000

    added link option

--- commands/citation.js
+++ commands/citation.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('citation')
-        .setDescription('gives a formatted citation for a book')
+        .setDescription('gives a formatted citation')
         .addStringOption(option => 
             option.setName('title')
-                .setDescription('Title of the book')
+                .setDescription('Title')
                 .setRequired(true))
         .addStringOption(option => 
             option.setName('surname')
@@ -18,26 +18,36 @@ module.exports = {
                 .setRequired(true))
         .addStringOption(option => 
             option.setName('year')
-                .setDescription('Year Published. If no year, leave blank')
+                .setDescription('Year Published.')
                 .setRequired(true))
-
+        .addStringOption(option => 
+            option.setName('link')
+                .setDescription('link to the webpage')
+                .setRequired(false))
         ,
     async execute(interaction) {
         let surname = interaction.options.getString('surname');
         let firstname = interaction.options.getString('firstname');
         let year = interaction.options.getString('year');
         let title = interaction.options.getString('title');
+        let link = interaction.options.getString("link")
 
         if (year === null){
             year = "n.d."
         }
+       
 
         let citation = surname + ", " + firstname.charAt(0) + ". (" + year + ") " + title
 
         let inText = "(" + surname + ", " + year + ")"
 
+        if (link){
+            citation = citation + " [online] [accessed on " + new Date().getDate() + "] " + link
+        }
+
         let reply = "reference list - " + citation + "\n in-text - " + inText 
         await interaction.reply(reply);
-        
+
+
     },
 };
