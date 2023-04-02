3926fbc167b3b39e12a6790c02945793f73844d6
jamie <me@skilet.ro>
2023-04-02 22:29:24 +0100

    remove tmp files

--- tmp2.js
+++
@@ -1,8 +0,0 @@
-const { startOfWeek, endOfWeek } = require("date-fns");
-
-const date = new Date;
-const start = startOfWeek(date, {weekStartsOn: 1});
-const end = endOfWeek(date, {weekStartsOn: 1});
-
-console.log(start)
-console.log(end)
